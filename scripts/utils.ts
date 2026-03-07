b98a0b5c1792d99abb0b63886e88cd5d670e87c7
Tanmay Band <tanmayband@gmail.com>
2026-03-07 18:17:11 +0530

    footer mods

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,4 +1,5 @@
 import { getCollection } from 'astro:content';
+import type { iconPaths } from  '../src/components/IconPaths';
 
 export function sortByCustomOrder<T extends { id: string; }>(
   items: T[],
@@ -34,9 +35,9 @@ export async function sortWork()
 }
 
 const testimonialsOrder = [
-    "hiddengem-gem-jam",
+    "80-lvl-3d-metasites",
     "hanx-101-trivia",
-    "80-lvl-3d-metasites"
+    "hiddengem-gem-jam"
 ];
 
 export async function sortTestimonials()
@@ -44,3 +45,34 @@ export async function sortTestimonials()
     const testimonialsEntries = await getCollection("testimonials");
     return sortByCustomOrder(testimonialsEntries, testimonialsOrder);
 }
+
+/** Icon links to social media — edit these with links to your profiles! */
+const contactDetails: { label: string; href: string; icon: keyof typeof iconPaths }[] = [
+  { 
+    label: 'Email',
+    href: 'mailto:tanmayband+hi@gmail.com',
+    icon: 'at-logo'
+  },
+  { 
+    label: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/tanmayband/',
+    icon: 'linkedin-logo'
+  },
+  { 
+    label: 'itch.io',
+    href: 'https://tanbanman.itch.io/',
+    icon: 'itch-io-logo'
+  },
+  { 
+    label: 'GitHub',
+    href: 'https://github.com/tanmayband',
+    icon: 'github-logo'
+  },
+  // { 
+  // label: 'YouTube',
+  // href: 'https://www.youtube.com/@me/',
+  // icon: 'youtube-logo'
+  // }
+];
+
+export default contactDetails;
